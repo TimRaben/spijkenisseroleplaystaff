76d94e17d7415278ebbcac31ff039ba97492ea2e
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-09-01 18:40:00 +0200

    init
    init

---
+++ commands/roleplayhost.js
@@ -0,0 +1,50 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    message.delete();
+
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(":x: **-** Sorry, jij kan geen Roleplay Hosten!");
+
+    if (!args[0]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
+
+    if (!args[1]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
+
+    if (!args[2]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
+
+    if (!args[3]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
+
+    if (!args[4]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
+
+    if (!args[5]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
+
+    if (!args[6]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
+
+    if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.reply("Geen perms");
+
+    var embed = new discord.MessageEmbed()
+            .setThumbnail("https://images-ext-2.discordapp.net/external/SFscr8WncU5Q09kCmenZ0gvMDK7FEdNRPDlAlIyuygg/%3Fsize%3D128/https/cdn.discordapp.com/icons/866238268277784586/a_a9ba29c9c1642df288042842c0070818.png")
+            .setTitle("Spijkenisse Roleplay - Roleplays")
+            .setDescription(`**\n• Datum:** ${args[0]}\n**• Tijd:** ${args[1]}\n\n**• Host:** ${args[2]}\n**• Co Host:** ${args[3]}\n\n**• Playstation Naam:** ${args[4]}\n\n**• Toevoegingen:** ${args.slice(6).join(" ")}\n\nTags: ${args[5]}`)
+            .setColor("AQUA")
+            .setFooter(`${message.member.displayName} • Spijkenisse Roleplay`)
+            .setTimestamp()
+
+        var channel = message.member.guild.channels.cache.get("866337102185365504");
+ 
+        if (!channel) return;
+
+    channel.send(embed);
+
+    var msg = await message.channel.send(embed)
+
+    await msg.react('✅');
+    await msg.react('⏰');
+    await msg.react('❌');
+
+}
+
+
+module.exports.help = {
+    name: "roleplay-host"
+}
---
+++ index.js
@@ -0,0 +1,99 @@
+const discord = require("discord.js");
+const botConfig = require("./botconfig.json");
+
+//  Command handler
+const fs = require("fs");
+const { isFunction } = require("util");
+
+const client = new discord.Client();
+
+
+//  Command handler
+client.commands = new discord.Collection();
+
+
+client.login(botConfig.token);
+
+
+//  Command handler
+fs.readdir("./commands/", (err, files) => {
+
+    if (err) console.log(err);
+
+    var jsFiles = files.filter(f => f.split(".").pop() === "js");
+
+    if (jsFiles.length <= 0) {
+        console.log("Kon geen files vinden");
+        return;
+    }
+
+    jsFiles.forEach((f, i) => {
+
+        var fileGet = require(`./commands/${f}`);
+        console.log(`De file ${f} is geladen`);
+
+        client.commands.set(fileGet.help.name, fileGet);
+    });
+
+});
+
+
+client.on("ready", async () => {
+
+    console.log(`${client.user.username} is online.`);
+
+    setInterval(() => {
+        const statuses = [
+            `Spelers`,
+            `Moderatie`,
+            `Spijkenisse Roleplay`,
+            `Roleplays`,
+            `Commands`,
+        ]
+    
+        const status = statuses[Math.floor(Math.random() * statuses.length)]
+        client.user.setActivity(status, { type: "WATCHING"}) // Can Be WATCHING, STREAMING, LISTENING
+    }, 4500) // Second You Want to Change Status, This Cahnges Every 2 Seconds
+
+});
+
+client.on("message", async message => {
+
+    if (message.author.bot) return;
+
+    if (message.channel.type === "dm") return;
+
+
+    // var msg = message.content.toLowerCase();
+
+    // for (let i = 0; i < swearWords["vloekwoorden"].length; i++) {
+
+    //     if (msg.includes(swearWords["vloekwoorden"][i])) {
+
+    //         message.delete();
+
+    //         return message.reply("Gelieve niet te vloeken").then(msg => msg.delete({ timeout: 3000 }));
+
+    //     }
+
+    // }
+
+
+    var prefix = botConfig.prefix;
+
+    var messageArray = message.content.split(" ");
+
+
+    var command = messageArray[0];
+
+    if (!message.content.startsWith(prefix)) return;
+
+    //  Command handler
+    var arguments = messageArray.slice(1);
+
+    var commands = client.commands.get(command.slice(prefix.length));
+
+
+    if (commands) commands.run(client, message, arguments);
+
+});
