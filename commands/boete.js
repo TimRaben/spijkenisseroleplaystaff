e328fe2c8ebfe5196ab5570f1fd03712dc37aeb6
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-09-01 21:48:30 +0200

    init10
    init10

--- commands/boete.js
+++ commands/boete.js
@@ -4,6 +4,15 @@ module.exports.run = async (client, message, args) => {
 
     message.delete();
 
+    if (!message.member.hasPermission("USE_SLASHCOMMANDS")) return message.reply("sorry jij kan dit niet");
+
+    if (!args[0]) return message.reply("Gebruik: ``spn/boete (Persoon) (Boete in getallen bijvoorbeeld 650, daarmee bedoel je 650 euro!) (Celstraf in Maanden) (Verwaardelijke Celstraf) (Strafbaar(e) feiten)");
+
+    if (!args[1]) return message.reply("Gebruik: ``spn/boete (Persoon) (Boete in getallen bijvoorbeeld 650, daarmee bedoel je 650 euro!) (Celstraf in Maanden) (Verwaardelijke Celstraf) (Strafbaar(e) feiten)");
+
+    if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.reply("Geen perms");
+
+
     var boeteEmbed = new discord.MessageEmbed()
             .setDescription(`Je hebt een Boete gekregen, hieronder vind je de informatie van de boete!\n\n**__**Informatie**__\n**Persoon:** ${args[0]}\n**Agent:** ${message.author}\n**Boete:** ${args[1]},- euro\n**Celstraf:** ${args[2]} Maanden\n**Celstraf waarvan Voorwaardelijk:** ${args[3]} Maanden\n**Strafbare Feiten:** ${args.slice(4).join(" ")}`)
             .setColor("RED")
