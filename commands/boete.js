6044706ec500cc9fb7583486f174df819e4eb854
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-09-01 22:39:19 +0200

    init22
    init22

--- commands/boete.js
+++ commands/boete.js
@@ -18,7 +18,7 @@ module.exports.run = async (client, message, args) => {
 
 
     var boeteEmbed = new discord.MessageEmbed()
-            .setDescription(`Je hebt een Boete gekregen, hieronder vind je de informatie van de boete!\n\n**__**Informatie**__\n**Persoon:** ${args[0]}\n**Agent:** ${message.author}\n**Boete:** ${args[1]},- euro\n**Celstraf:** ${args[2]} Maanden\n**Celstraf waarvan Voorwaardelijk:** ${args[3]} Maanden\n**Strafbare Feiten:** ${args.slice(4).join(" ")}`)
+            .setDescription(`Je hebt een Boete gekregen, hieronder vind je de informatie van de boete!\n\n__**Informatie**__\n**Persoon:** ${args[0]}\n**Agent:** ${message.author}\n**Boete:** ${args[1]},- euro\n**Celstraf:** ${args[2]} Maanden\n**Celstraf waarvan Voorwaardelijk:** ${args[3]} Maanden\n**Strafbare Feiten:** ${args.slice(4).join(" ")}`)
             .setColor("RED")
             .setTitle("Spijkenisse Roleplay - Boete Systeem")
             .setTimestamp()
