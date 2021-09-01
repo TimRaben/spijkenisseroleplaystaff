7dbb66e8732fbbb422491fd0c8d3c0eef05c2b4d
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-09-01 22:34:07 +0200

    init20
    init20

--- commands/boete.js
+++ commands/boete.js
@@ -4,8 +4,6 @@ module.exports.run = async (client, message, args) => {
 
     message.delete();
 
-    if (!message.member.hasPermission("USE_APPLICATION_COMMANDS")) return message.reply("Jij bent GEEN Politie en hebt dus niet de juiste machtigingen om boetes uit te schrijven!");
-
     if (!args[0]) return message.reply("Gebruik: ``spn/boete (Persoon) (Boete in getallen bijvoorbeeld 650, daarmee bedoel je 650 euro!) (Celstraf in Maanden) (Verwaardelijke Celstraf) (Strafbaar(e) feiten)");
 
     if (!args[1]) return message.reply("Gebruik: ``spn/boete (Persoon) (Boete in getallen bijvoorbeeld 650, daarmee bedoel je 650 euro!) (Celstraf in Maanden) (Verwaardelijke Celstraf) (Strafbaar(e) feiten)");
