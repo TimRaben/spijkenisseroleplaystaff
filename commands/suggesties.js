46f02907885f71875a109358fee49ad65d9abf82
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-09-03 23:04:07 +0200

    init41
    init41

---
+++ commands/suggesties.js
@@ -0,0 +1,49 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    message.delete();
+
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(":x: **-** Sorry, jij kan geen Roleplay Hosten!");
+
+    if (!args[0]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
+
+    if (!args[1]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
+
+    if (!args[2]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
+
+    if (!args[3]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
+
+    if (!args[4]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
+
+    if (!args[5]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
+
+    if (!args[6]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
+
+    if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.reply("Geen perms");
+
+    var embed = new discord.MessageEmbed()
+            .setThumbnail(member.user.displayAvatarURL({ dynamic: true}))
+            .setTitle("Spijkenisse Roleplay - Suggesties")
+            .setDescription(`**Suggestie:**\n ${args.slice(0).join(" ")}\n**Ingezonden door:**\n${message.member}`)
+            .setColor("AQUA")
+            .setFooter(`https://images-ext-2.discordapp.net/external/SFscr8WncU5Q09kCmenZ0gvMDK7FEdNRPDlAlIyuygg/%3Fsize%3D128/https/cdn.discordapp.com/icons/866238268277784586/a_a9ba29c9c1642df288042842c0070818.png${message.member.displayName} • Spijkenisse Roleplay`)
+            .setTimestamp()
+
+        var channel = message.member.guild.channels.cache.get("866337040759783454");
+ 
+        if (!channel) return;
+
+    channel.send(embed);
+
+    var msg = await message.channel.send(embed)
+
+    await msg.react('✅');
+    await msg.react('❌');
+
+}
+
+
+module.exports.help = {
+    name: "suggestie"
+}
