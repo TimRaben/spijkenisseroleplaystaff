2ac1ce1a2c89b22cdae1be6ad889de83d9582aaf
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-09-03 23:34:26 +0200

    init58
    init58

--- commands/suggesties.js
+++ commands/suggesties.js
@@ -19,8 +19,8 @@ module.exports.run = async (client, message, args) => {
             .setTimestamp()
 
         var channel = message.member.guild.channels.cache.get("866337040759783454");
- 
-        if (!channel) return;
+
+    channel.send(embed);
 
     var msg = await message.channel.send(embed)
 
