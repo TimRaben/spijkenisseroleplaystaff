db8d5e1c98746a9cab030dc5031b6a2dacba36bc
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-09-04 00:20:32 +0200

    init84
    init84

--- commands/suggesties.js
+++ commands/suggesties.js
@@ -6,29 +6,17 @@ module.exports.run = async (client, message, args) => {
 
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(":x: **-** Sorry, jij kan geen Roleplay Hosten!");
 
-    if (!args[0]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
-
-    if (!args[1]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
-
-    if (!args[2]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
-
-    if (!args[3]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
-
-    if (!args[4]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
-
-    if (!args[5]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
-
-    if (!args[6]) return message.reply(":x: **-** Gebruik: ``spn/roleplay-host (Datum) (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)``");
+    if (!args[0]) return message.reply(":x: **-** Geef een suggestie op!");
 
     if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.reply("Geen perms");
 
     var embed = new discord.MessageEmbed()
-        .setThumbnail(`${message.author.displayAvatarURL({ dynamic: true})}`)
-        .setTitle("Spijkenisse Roleplay - Suggesties")
-        .setDescription(`**Suggestie:**\n${args.slice(0).join(" ")}\n**Ingezonden door:**\n${message.author}\n\n*Laat jou suggestie achter met ``spn/suggestie (suggestie)```)
-        .setColor("RANDOM")
-        .setFooter(`${message.member.displayName} • Spijkenisse Roleplay`)
-        .setTimestamp()
+            .setThumbnail(member.user.displayAvatarURL({ dynamic: true}))
+            .setTitle("Spijkenisse Roleplay - Suggesties")
+            .setDescription(`**Suggestie:**\n ${args.slice(0).join(" ")}\n**Ingezonden door:**\n${message.member}\n\n*Laat jou suggestie achter met* spn/suggestie (suggestie)`)
+            .setColor("AQUA")
+            .setFooter(`https://images-ext-2.discordapp.net/external/SFscr8WncU5Q09kCmenZ0gvMDK7FEdNRPDlAlIyuygg/%3Fsize%3D128/https/cdn.discordapp.com/icons/866238268277784586/a_a9ba29c9c1642df288042842c0070818.png${message.member.displayName} • Spijkenisse Roleplay`)
+            .setTimestamp()
 
         var channel = message.member.guild.channels.cache.get("866337040759783454");
  
