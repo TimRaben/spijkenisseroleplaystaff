e83385581d0bb184e4999928587c9675fb42db66
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-09-09 13:30:35 +0200

    init137
    init137

--- commands/info.js
+++ commands/info.js
@@ -3,15 +3,11 @@ const discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
 
     var serverEmbed = new discord.MessageEmbed()
-            .setDescription("Zie hier de Informatie van deze Server")
+            .setDescription("Zie hier de Informatie van deze Server!\n\n__Custom BOT:__ \nSpijkenisse Roleplay - Staff\n\n__Server ID:__\n866238268277784586\n\n__Server Eigenaar:__\nRecon Pro NL#1000\n\n__Server Aangemaakt op:__\n18 July 2021\n\n")
             .setColor("#kleur")
             .setThumbnail("https://images-ext-2.discordapp.net/external/SFscr8WncU5Q09kCmenZ0gvMDK7FEdNRPDlAlIyuygg/%3Fsize%3D128/https/cdn.discordapp.com/icons/866238268277784586/a_a9ba29c9c1642df288042842c0070818.png")
-            .addField("**Custom BOT**", client.user.username)
             .addField("**Je bent deze server gejoind op:**", message.member.joinedAt)
             .addField("**Totale Members:**", message.guild.memberCount)
-            .addField("**Server Gemaakt op:**\n18 July 2021")
-            .addField("**Server Owner:**\nRecon Pro NL#1000")
-            .addField("**Server ID:**\n866238268277784586")
             .setFooter("Spijkenisse Roleplay")
             .setTimestamp()
 
