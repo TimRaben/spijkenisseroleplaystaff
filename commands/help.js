20ad5f3dc4d8fe1a588c5c106539b34dada33c24
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-09-09 10:23:21 +0200

    init136
    init136

--- commands/help.js
+++ commands/help.js
@@ -3,11 +3,11 @@ const discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
 
     var helpEmbed = new discord.MessageEmbed()
-            .setDescription(`__**Staff Commando's**__\n**spn/ban (persoon) (reden) -** Verban iemand.\n**spn/warn (persoon (reden) -** Geef iemand een Warn.\n**spn/kick (persoon) (reden) -** Verwijder iemand.\n**spn/roleplay-host (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen) -** Host een Roleplay.\n**spn/clear (aantal) -** Verwijder een bepaald aantal berichten.\n**spn/giveaway (winnercount) (tijd) (item) -** Host een Giveaway\n\n__**Algemene Commando's**__\n**spn/help -** Zie dit menu.\n**spn/info -** Krijg Server Info.\n**spn/ping -** Zie de ms snelheid van de BOT.\n**spn/say (bericht) -** Stuur een Commando via de BOT.\n**spn/suggestie (suggestie) -** Geef een suggestie. `)
+            .setDescription(`__Staff Commando's__\n**spn/ban (persoon) (reden** - Verban iemand.\n**spn/warn (persoon (reden)** - Geef iemand een Warn.\n**spn/kick (persoon) (reden)** - Verwijder iemand.\n**spn/roleplay-host (Tijd) (Host) (Co Host) (PSN Naam) (Tags) (Toevoegingen)** - Host een Roleplay.\n**spn/clear (aantal)** - Verwijder een bepaald aantal berichten.\n**spn/giveaway (winnercount) (tijd) (item)** - Host een Giveaway\n\n__Algemene Commando's__\n**spn/help** - Zie dit menu.\n**spn/info** - Krijg Server Info.\n**spn/ping** - Zie de ms snelheid van de BOT.\n**spn/say (bericht)** - Stuur een Commando via de BOT.\n**spn/suggestie (suggestie)** - Geef een suggestie. `)
             .setColor("ORANGE")
             .setTitle("Spijkenisse Roleplay - Help Menu")
             .setThumbnail("https://images-ext-2.discordapp.net/external/SFscr8WncU5Q09kCmenZ0gvMDK7FEdNRPDlAlIyuygg/%3Fsize%3D128/https/cdn.discordapp.com/icons/866238268277784586/a_a9ba29c9c1642df288042842c0070818.png")
-            .setFooter("Spijkenisse Roleplay • Help Menu")
+        .setFooter(`${message.author.displayAvatarURL({ dynamic: true})} Spijkenisse Roleplay • Help Menu`)
             .setTimestamp()
 
         return message.channel.send(helpEmbed);
--- commands/info.js
+++ commands/info.js
@@ -2,18 +2,20 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    var botEmbed = new discord.MessageEmbed()
-            .setTitle('Een titel')
-            .setDescription("Zet de beschrijving")
-            .setColor("#0099ff")
-            .addField("Bot naam", client.user.username)
-
-            .setThumbnail('https://i.imgur.com/wSTFkRM.png')
-            .setImage('https://i.imgur.com/wSTFkRM.png')
+    var serverEmbed = new discord.MessageEmbed()
+            .setDescription("Zie hier de Informatie van deze Server")
+            .setColor("#kleur")
+            .setThumbnail("https://images-ext-2.discordapp.net/external/SFscr8WncU5Q09kCmenZ0gvMDK7FEdNRPDlAlIyuygg/%3Fsize%3D128/https/cdn.discordapp.com/icons/866238268277784586/a_a9ba29c9c1642df288042842c0070818.png")
+            .addField("**Custom BOT**", client.user.username)
+            .addField("**Je bent deze server gejoind op:**", message.member.joinedAt)
+            .addField("**Totale Members:**", message.guild.memberCount)
+            .addField("**Server Gemaakt op:**\n18 July 2021")
+            .addField("**Server Owner:**\nRecon Pro NL#1000")
+            .addField("**Server ID:**\n866238268277784586")
+            .setFooter("Spijkenisse Roleplay")
             .setTimestamp()
-            .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
 
-        return message.channel.send(botEmbed);
+        return message.channel.send(serverEmbed);
 
 }
 
