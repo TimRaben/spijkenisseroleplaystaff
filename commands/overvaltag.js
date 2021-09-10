ce963eb4be23bcbb0f4a83aeae94584f8ba88795
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-09-10 08:55:13 +0200

    init146
    init146

--- commands/overvaltag.js
+++ commands/overvaltag.js
@@ -1,10 +1,15 @@
+const discord = require("discord.js");
+
 module.exports.run = async (client, message, args) => {
 
-    if (!args[0])
 
-    if (!args[1])
+    message.delete();
 
-    return message.channel.send(`<@&866336951632003072> <@&866336897902575646> <@$866336909693550612>`);
+    if (!args[0]);
+
+    if (!args[1]);
+
+    return message.channel.send(`<@&866336951632003072> <@&866336897902575646> <@&866336909693550612>`);
 
 
 }
