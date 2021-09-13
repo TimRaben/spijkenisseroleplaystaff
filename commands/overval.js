fbde9287f1fe6a6b1876b900dce1f24abf7c931e
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-09-13 08:30:33 +0200

    init165
    init165

--- commands/overval.js
+++ commands/overval.js
@@ -10,12 +10,12 @@ module.exports.run = async (client, message, args) => {
 
     var overvalEmbed = new discord.MessageEmbed()
             .setTitle("Spijkenisse Roleplay - Overval")
-            .setDescription(`**\n**Er is een overval gestart! Bekijk hier onder de nodige Informatie!\n\n__Informatie:__\n**Type Overval:** ${args[0]}\n**Discord Naam:** ${message.members.displayName}\n**Playstation naam:** ${args[1]}\n**Tijd van melding:** ${message.createdTimestamp}\n**Hulpdiensten:** Politie, DSI & KMAR`)
+            .setDescription(`**\n**Er is een overval gestart! Bekijk hier onder de nodige Informatie!\n\n__Informatie:__\n**Type Overval:** ${args[0]}\n**Discord Naam:** ${message.member.author}\n**Playstation naam:** ${args[1]}\n**Tijd van melding:** ${message.createdTimestamp}\n**Hulpdiensten:** Politie, DSI & KMAR`)
             .setTimestamp()
             .setFooter("Spijkenisse Roleplay • Overval")
             .setColor("LIGHTRED")
 
-        return message.channel.send(overvalEmbed, `<&866336898400911361> <&866336951632003072> <&866336909693550612>`);
+        return message.channel.send(overvalEmbed, '<&866336898400911361> <&866336951632003072> <&866336909693550612>');
 }
 
     module.exports.help = {
