68a6075c85842bfac8d23f9063aaad7bc79a6d7a
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-10-13 12:37:13 +0200

    init199
    init199

--- index.js
+++ index.js
@@ -45,10 +45,10 @@ client.on("ready", async () => {
     setInterval(() => {
         const statuses = [
             `🎫 - Tickets`,
-            `💬 - ${message.guild.channelCount} Kanalen`,
+            `💬 - ${guild.channelCount} Kanalen`,
             `💳 - ID's | spn/id`,
             `🔰 - 14 Staffleden`,
-            `👥 - ${message.guild.memberCount} Leden`,
+            `👥 - ${guild.memberCount} Leden`,
         ]
     
         const status = statuses[Math.floor(Math.random() * statuses.length)]
