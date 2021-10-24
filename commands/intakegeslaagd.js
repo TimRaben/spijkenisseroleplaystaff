9ed14c233532c9475316e0c3041cf3913642809b
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-10-24 23:57:46 +0200

    init301
    init301

---
+++ commands/intakegeslaagd.js
@@ -0,0 +1,19 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('⛔ **|** Je hebt niet de juiste bevoegdheid om iemand te whitelisten.')
+        const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+        if(!Member) return message.channel.send('⛔ **|** Persoon niet gevonden!')
+    
+        let role2 = message.guild.roles.cache.find(r => r.name.toLowerCase() === '» 🏡 | Inwoner Spijkenisse', '———INWONER———', '» 🌈 | Whitelisted', '———WHITELISTED———')
+        if(Member.roles.cache.has(role2.id)) return message.channel.send(`${Member.displayName} is al gewhitelist.`)
+        await Member.roles.add(role2)
+        message.channel.send(`${Member.displayName} is succesvol gewhitelist.`)
+    }
+
+module.exports.help = {
+    name: "whitelist",
+    description: "Ban iemand",
+    category: "Algemeen"
+}
