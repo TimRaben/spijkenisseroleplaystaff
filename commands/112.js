ebefe850d1613bf4b7dd19664f93362894a83714
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-11-03 15:26:13 +0100

    init1
    init1

--- commands/112.js
+++ commands/112.js
@@ -9,7 +9,7 @@ module.exports.run = async (client, message, args) => {
 
     var meldingBestaat = false;
 
-    message.guild.channels.cache.array.forEach(channel => {
+    message.guild.channels.cache.forEach(channel => {
 
         if (channel.name == userName.toLowerCase() + "-" + userDiscriminator) {
             meldingBestaat = true;
