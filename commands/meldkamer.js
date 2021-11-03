74525fb3f5e5d07c7bbff1fbf8a3a0be69da618f
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-11-03 13:25:35 +0100

    init1
    init1

--- commands/meldkamer.js
+++ commands/meldkamer.js
@@ -8,12 +8,15 @@ module.exports.run = async (client, message, args) => {
 
     if (!args[1]) return message.reply(":x: - Geef een oorzaak van een melding op! Dus laat de reden weten waarom je een 112 melding wilt maken.")
 
+    var meldkamericon = 'https://pbs.twimg.com/profile_images/746720808454717440/Mt-tSDgi_400x400.jpg'
+
     var meldkamerEmbed = new discord.MessageEmbed()
             .setTitle("Spijkenisse Roleplay - Meldkamer")
+            .setAuthor(`${meldkamericon} Justitie - Meldkamer`)
             .setDescription(`**112 Melding**\n\nEr is een melding binnengekomen bij de meldkamer! Bekijk hier onder de nodige Informatie! De Hulpdiensten zijn z.s.m. onderweg! Heb nog even geduld!\n\n__Informatie:__\n**Locatie (PSN):** ${args[0]}\n**Discord Naam:** ${message.author}\n**Playstation naam:** ${args.slice(1).join(" ")}\n**Hulpdiensten:** Politie\n\nEen melding kan gemaakt worden met *spn/112 (playstation naam) (melding)*`)
             .setThumbnail("https://pbs.twimg.com/profile_images/746720808454717440/Mt-tSDgi_400x400.jpg")
             .setTimestamp()
-            .setFooter("Spijkenisse Roleplay • Overval • Alle Rechten Voorbehoud")
+            .setFooter("Spijkenisse Roleplay • Meldkamer • Alle Rechten Voorbehoud")
             .setColor("RED")
 
         return message.channel.send('<@&866336898400911361>', meldkamerEmbed);
