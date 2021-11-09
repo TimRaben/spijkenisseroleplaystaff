47d8f2ffad050a82cfa4c76187b40722ad3d637f
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-11-09 15:03:45 +0100

    init1
    init1

--- index.js
+++ index.js
@@ -53,35 +53,7 @@ client.on("ready", async () => {
         client.user.setActivity(status, { type: "WATCHING"}) // Can Be WATCHING, STREAMING, LISTENING
     }, 4500) // Second You Want to Change Status, This Cahnges Every 2 Seconds
 
-});
-
-client.on('guildMemberAdd', member => {
-
-    let button = new MessageButton()
-            .setStyle('url')
-            .setURL('https://sway.office.com/QJVFr8icY5GWzJgd?ref=Link2')
-            .setLabel('📚 Regels')
-            .setID('click_to_function');
-
-    let button1 = new MessageButton()
-            .setStyle('url')
-            .setURL('https://spijkenisseroleplay.craftingstore.net/')
-            .setLabel('🛒 Store')
-            .setID('click_to_function');
-
-    var embed = new discord.MessageEmbed()
-        .setTitle(`Welkom <@${guildMember.user.id}>`)
-        .setAuthor("Spijkenisse Roleplay")
-        .setDescription(`Hey <@${guildMember.user.id}>, leuk dat je onze Server bent gejoined! Hopelijk heb je een fijne en leuke tijd op onze Server!\n\nLees wel even snel de regels door :wink:\n\n**Totale Members:** ${message.guild.memberCount}`)
-        .setThumbnail("https://media.discordapp.net/attachments/866336989891264532/905036514910695454/static_15.png?width=205&height=205")
-        .setFooter("Spijkenisse Roleplay • Welkom • Alle Rechten Voorbehoud")
-        .setTimestamp();
-
-        var channel = message.guild.channels.cache.get("902193674576863292");
-
-    channel.send(embed, button, button1);
-
-})
+});               
 
 client.on("message", async message => {
 
@@ -123,4 +95,32 @@ client.on("message", async message => {
     if (commands) commands.run(client, message, arguments);
 
 });
+
+client.on('guildMemberAdd', member => {
+
+    let button = new MessageButton()
+            .setStyle('url')
+            .setURL('https://sway.office.com/QJVFr8icY5GWzJgd?ref=Link2')
+            .setLabel('📚 Regels')
+            .setID('click_to_function');
+
+    let button1 = new MessageButton()
+            .setStyle('url')
+            .setURL('https://spijkenisseroleplay.craftingstore.net/')
+            .setLabel('🛒 Store')
+            .setID('click_to_function');
+
+    var embed = new discord.MessageEmbed()
+        .setTitle(`Welkom <@${guildMember.user.id}>`)
+        .setAuthor("Spijkenisse Roleplay")
+        .setDescription(`Hey <@${guildMember.user.id}>, leuk dat je onze Server bent gejoined! Hopelijk heb je een fijne en leuke tijd op onze Server!\n\nLees wel even snel de regels door :wink:\n\n**Totale Members:** ${message.guild.memberCount}`)
+        .setThumbnail("https://media.discordapp.net/attachments/866336989891264532/905036514910695454/static_15.png?width=205&height=205")
+        .setFooter("Spijkenisse Roleplay • Welkom • Alle Rechten Voorbehoud")
+        .setTimestamp();
+
+        var channel = message.guild.channels.cache.get("902193674576863292");
+
+    channel.send(embed, button);
+
+}) 
 
